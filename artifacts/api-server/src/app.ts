8096207c53e5b3001c0c859e3ead65973dd1f4ab
Harri082811 <harriwalk133@gmail.com>
2026-04-24 17:29:58 -0400

    d

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,31 +1,15 @@
 // @ts-nocheck
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
 import router from "./routes";
-import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req: any) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res: any) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  })
-);
+// Basic logger that doesn't require the broken pino-http library
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 app.use(cors());
 app.use(express.json());
